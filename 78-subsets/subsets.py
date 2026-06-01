3777c7105f9b5947e1503c613ba313346b89bd6c
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-06-01 17:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.27%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,13 +1,28 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        total_subset=1<<n  #2^n
         result=[]
-        for num in range(total_subset):
-            temp=[]
-            for i in range(n):
-                if num&(1<<i)!=0:
-                    temp.append(nums[i])
+        def func(index,subset):
+            if index>=len(nums):
+                result.append(subset.copy())
+                return
+                
+            subset.append(nums[index])
+            func(index+1,subset)
+
+            subset.pop()
+            func(index+1,subset)
+        
+        func(0,[])
+        return result
+            
+        # n=len(nums)
+        # total_subset=1<<n  #2^n
+        # result=[]
+        # for num in range(total_subset):
+        #     temp=[]
+        #     for i in range(n):
+        #         if num&(1<<i)!=0:
+        #             temp.append(nums[i])
                
-            result.append(temp)
-        return result   #TC=O(n*2^n) and SC = O(n*2^n)
+        #     result.append(temp)
+        # return result   #TC=O(n*2^n) and SC = O(n*2^n)
