0cd9354f29a638e3255173b38f86e2aa778d75f8
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-06-03 20:49:02 +0530

    Time: 4 ms (69.84%) | Memory: 19.6 MB (42.44%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -11,26 +11,28 @@ class Solution:
             for i in range(index,len(candidates)):
                 if i>index and candidates[i-1]==candidates[i]:
                     continue
+                if candidates[i]>total:
+                    break
                 subset.append(candidates[i])
                 backTrack(i+1,total-candidates[i],subset)
                 subset.pop()
         backTrack(0,target,[])
         return result
-    # bruteforce 
-    # def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-    #     candidates.sort()
-    #     result=set()
-    #     self.func(candidates,target,result,0,0,[])
-    #     return [list(x) for x in result]
-    
-    # def func(self,nums,target,result,index,total,subset):
-    #     if total==target:
-    #         result.add(tuple(subset))
-    #         return
-    #     if total>target or index>=len(nums):
-    #         return
-    #     subset.append(nums[index])
-    #     self.func(nums,target,result,index+1,total+nums[index],subset)
-    #     subset.pop()
-    #     self.func(nums,target,result,index+1,total,subset)
+        # bruteforce 
+        # def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        #     candidates.sort()
+        #     result=set()
+        #     self.func(candidates,target,result,0,0,[])
+        #     return [list(x) for x in result]
         
+        # def func(self,nums,target,result,index,total,subset):
+        #     if total==target:
+        #         result.add(tuple(subset))
+        #         return
+        #     if total>target or index>=len(nums):
+        #         return
+        #     subset.append(nums[index])
+        #     self.func(nums,target,result,index+1,total+nums[index],subset)
+        #     subset.pop()
+        #     self.func(nums,target,result,index+1,total,subset)
+            
