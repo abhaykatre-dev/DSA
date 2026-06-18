72ed2ca696c0f19a2574834ba08e301d3ddc8798
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-06-18 21:29:31 +0530

    Time: 2 ms (17.73%) | Memory: 19.4 MB (64.64%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,22 +1,48 @@
+from copy import deepcopy
+from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        #  using queue
         if image[sr][sc]==color:
             return image
         row=len(image)
         col=len(image[0])
         visited=deepcopy(image)
-        intialcolor=image[sr][sc]
-        def func(i,j):
-            if i<0 or i==row or j<0 or j==col:
-                return
-            if visited[i][j]!=intialcolor:
-                return
-            if visited[i][j]==color:
-                return
+        intial_color=visited[sr][sc]
+        queue=deque()
+        queue.append([sr,sc])
+        while queue:
+            i,j=queue.popleft()
             visited[i][j]=color
-            func(i+1,j)
-            func(i-1,j)
-            func(i,j+1)
-            func(i,j-1)
-        func(sr,sc)
+            for x,y in [[1,0],[-1,0],[0,1],[0,-1]]:
+                new_i=i+x
+                new_j=j+y
+                if new_i<0 or new_i==row or new_j<0 or new_j==col:
+                    continue
+                if visited[new_i][new_j]!=intial_color:
+                    continue
+                queue.append([new_i,new_j])
         return visited
+
+
+        # using bfs
+#         if image[sr][sc]==color:
+#             return image
+#         row=len(image)
+#         col=len(image[0])
+#         visited=deepcopy(image)
+#         intialcolor=image[sr][sc]
+#         def func(i,j):
+#             if i<0 or i==row or j<0 or j==col:
+#                 return
+#             if visited[i][j]!=intialcolor:
+#                 return
+#             if visited[i][j]==color:
+#                 return
+#             visited[i][j]=color
+#             func(i+1,j)
+#             func(i-1,j)
+#             func(i,j+1)
+#             func(i,j-1)
+#         func(sr,sc)
+#         return visited
