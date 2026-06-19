906ba4d0296db8122cd2f7c4e55fb305c7ed7244
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-06-20 01:11:42 +0530

    Time: 50 ms (93.57%) | Memory: 21.7 MB (96.37%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -9,27 +9,27 @@ class Solution:
         #top and bottom
         for i in range(col):
             if grid[0][i]==1:
+                grid[0][i]=0
                 queue.append((0,i))
             if grid[row-1][i]==1:
+                grid[row-1][i]=0
                 queue.append((row-1,i))
         #left and right
         for i in range(row):
             if grid[i][0]==1:
+                grid[i][0]=0
                 queue.append((i,0))
             if grid[i][col-1]==1:
+                grid[i][col-1]=0
                 queue.append((i,col-1))
         while queue:
             i,j=queue.popleft()
-            grid[i][j]=0
             for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
                 new_i=i+dx
                 new_j=j+dy
-                if new_i<0 or new_i>=row or new_j<0 or new_j>=col:
-                    continue
-                if grid[new_i][new_j]!=1:
-                    continue
-                grid[new_i][new_j]=0
-                queue.append((new_i,new_j))
+                if 0<=new_i<row and 0<=new_j<col and grid[new_i][new_j]==1:
+                    grid[new_i][new_j]=0
+                    queue.append((new_i,new_j))
         move=0
         for i in range(row):
             for j in range(col):
