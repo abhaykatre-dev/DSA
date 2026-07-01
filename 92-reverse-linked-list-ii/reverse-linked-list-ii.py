eec2c6c10d250c18a8dd6a83bcf22c7a66470950
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-07-01 21:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (24.61%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,21 +5,39 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        temp=head
-        result=[]
-        while temp:
-            result.append(temp.val)
-            temp=temp.next
-        left-=1
-        right-=1
-        while left<right:
-            result[left],result[right]=result[right],result[left]
-            left+=1
-            right-=1
-        temp=head
-        count=0
-        while temp:
-            temp.val=result[count]
-            count+=1
-            temp=temp.next
-        return head
+        dummynode=ListNode(0)
+        dummynode.next=head
+        leftprev=dummynode
+        curr=head
+        for _ in range(left-1):
+            leftprev=curr
+            curr=curr.next
+        prev=None
+        for _ in range(right-left+1):
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
+        leftprev.next.next=curr
+        leftprev.next=prev
+        return dummynode.next
+        
+
+        # temp=head
+        # result=[]
+        # while temp:
+        #     result.append(temp.val)
+        #     temp=temp.next
+        # left-=1
+        # right-=1
+        # while left<right:
+        #     result[left],result[right]=result[right],result[left]
+        #     left+=1
+        #     right-=1
+        # temp=head
+        # count=0
+        # while temp:
+        #     temp.val=result[count]
+        #     count+=1
+        #     temp=temp.next
+        # return head
