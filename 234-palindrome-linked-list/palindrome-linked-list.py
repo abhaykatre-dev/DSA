bc7f1e686299a6d5c56520eebb507476aecd3282
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-07-01 15:32:01 +0530

    Time: 44 ms (25.36%) | Memory: 42.6 MB (64.64%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -10,33 +10,16 @@ class Solution:
             slow=slow.next
             fast=fast.next.next
         prev=None
-        curr=slow
-        while curr:
-            nxt=curr.next
-            curr.next=prev
-            prev=curr
-            curr=nxt
+        while slow:
+            temp=slow.next
+            slow.next=prev
+            prev=slow
+            slow=temp
         left=head
         right=prev
-
         while left and right:
             if left.val!=right.val:
                 return False
             left=left.next
             right=right.next
         return True
-
-        #brute-force
-        # temp=head
-        # myList=[] 
-        # while temp:
-        #     myList.append(temp.val)
-        #     temp=temp.next
-        # st=0
-        # end=len(myList)-1    
-        # while st<end:
-        #     if myList[st]!=myList[end]:
-        #         return False
-        #     st+=1
-        #     end-=1
-        # return True
