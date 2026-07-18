56a2eb7f453049a1bfba426d6c84928e1493b56e
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-07-19 01:39:54 +0530

    Time: 7 ms (74.32%) | Memory: 21.2 MB (24.27%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,18 +1,35 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n=len(height)
-        leftmax=[0]*n
-        leftmax[0]=height[0]
-        for i in range(1,n):
-            leftmax[i]=max(leftmax[i-1],height[i])
-        rightmax=[0]*n
-        rightmax[n-1]=height[n-1]
-        for i in range(n-2,-1,-1):
-            rightmax[i]=max(rightmax[i+1],height[i])
+        if not height:
+            return 0
+        l,r=0,len(height)-1
+        l_max=r_max=0
+        water=0
+        while l<r:
+            l_max=max(height[l],l_max)
+            r_max=max(height[r],r_max)
+            if l_max<r_max:
+                water+=l_max-height[l]
+                l+=1
+            else:
+                water+=r_max-height[r]
+                r-=1
+        return water
+
+
+        # n=len(height)
+        # leftmax=[0]*n
+        # leftmax[0]=height[0]
+        # for i in range(1,n):
+        #     leftmax[i]=max(leftmax[i-1],height[i])
+        # rightmax=[0]*n
+        # rightmax[n-1]=height[n-1]
+        # for i in range(n-2,-1,-1):
+        #     rightmax[i]=max(rightmax[i+1],height[i])
         
-        TP=0
-        for i in range(n):
-            TP+=min(leftmax[i],rightmax[i])-height[i]
-        return TP
+        # TP=0
+        # for i in range(n):
+        #     TP+=min(leftmax[i],rightmax[i])-height[i]
+        # return TP
         
         
