f8003da23da5333faeee9f87ae997f51ed618787
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-07-21 01:46:39 +0530

    Time: 7 ms (58.47%) | Memory: 19.5 MB (82.23%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.py
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -2,15 +2,24 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m = len(grid)
         n = len(grid[0])
-        for _ in range(k):
-            ans = [[0] * n for _ in range(m)]
-            for i in range(m):
-                for j in range(n):
-                    if i < m - 1 and j == n - 1:
-                        ans[i + 1][0] = grid[i][j]
-                    elif i == m - 1 and j == n - 1:
-                        ans[0][0] = grid[i][j]
-                    else:
-                        ans[i][j + 1] = grid[i][j]
-            grid = ans
-        return grid
+        ans=[[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                index=i*n+j
+                new_index=(index+k)%(m*n)
+                row=new_index//n
+                col=new_index%n
+                ans[row][col]=grid[i][j]
+        return ans
+        # for _ in range(k):
+        #     ans = [[0] * n for _ in range(m)]
+        #     for i in range(m):
+        #         for j in range(n):
+        #             if i < m - 1 and j == n - 1:
+        #                 ans[i + 1][0] = grid[i][j]
+        #             elif i == m - 1 and j == n - 1:
+        #                 ans[0][0] = grid[i][j]
+        #             else:
+        #                 ans[i][j + 1] = grid[i][j]
+        #     grid = ans
+        # return grid
