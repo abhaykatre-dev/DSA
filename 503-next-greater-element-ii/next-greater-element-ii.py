314a1d7ee5bde77ca68575144340a53a83a65f24
Abhay Katre <166499942+abhaykatre-dev@users.noreply.github.com>
2026-08-15 08:34:08 +0530

    Time: 35 ms (17.95%) | Memory: 20.9 MB (86.39%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -4,14 +4,9 @@ class Solution:
         ans=[-1]*n
         stack=[]
         for i in range(2*n-1,-1,-1):
-            # [1,2,3,4,3,1,2,3,4,3]
-            # To access index 9 -> 9 % 5 = 4
-            # nums[4] = 3
-            while stack and stack[-1]<=nums[i%n]: 
+            while stack and stack[-1]<=nums[i%n]:
                 stack.pop()
-            if i<n and stack:
-                ans[i]=stack[-1]
+            if stack:
+                ans[i%n]=stack[-1]
             stack.append(nums[i%n])
         return ans
-
-           
